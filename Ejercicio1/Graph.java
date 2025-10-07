36de5282df255027c0b013d19d7099c1c878ccc8
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-10-06 22:55:50 -0500

    haciendo una funcion para generar aleatoriamente un grafo

--- Ejercicio1/Graph.java
+++ Ejercicio1/Graph.java
@@ -1,5 +1,5 @@
 package Ejercicio1;
-
+import java.util.Random;
 import java.util.List;
 
 public class Graph {
@@ -34,7 +34,7 @@ public class Graph {
         }
         return null;
     }
-    
+
     public void showGraph() {
         System.out.println("Nodos:");
         for (Node n : nodes) {
@@ -43,6 +43,45 @@ public class Graph {
         System.out.println("Aristas:");
         for (Edge e : edges) {
             System.out.println(" - " + e);
+        }
+    }
+
+    public void generarGrafoAleatorio(int cantidadNodos, int cantidadAristas, boolean ponderado) {
+        Random rand = new Random();
+
+        // Crear los nodos
+        for (int i = 0; i < cantidadNodos; i++) {
+            addNode("N" + i); // los nombres serán N0, N1, N2...
+        }
+
+        // Crear las aristas
+        int totalIntentos = 0;
+        while (edges.size() < cantidadAristas && totalIntentos < cantidadAristas * 5) {
+            int i = rand.nextInt(cantidadNodos);
+            int j = rand.nextInt(cantidadNodos);
+
+            if (i == j) { // evitar lazos a sí mismo
+                totalIntentos++;
+                continue;
+            }
+
+            Node from = getNode("N" + i);
+            Node to = getNode("N" + j);
+
+            // evitar duplicados 
+            boolean existe = false;
+            for (Edge e : edges) {
+                if (e.getFrom().equals(from) && e.getTo().equals(to)) {
+                    existe = true;
+                    break;
+                }
+            }
+
+            if (!existe) {
+                int peso = ponderado ? (rand.nextInt(9) + 1) : 0; // peso aleatorio 1–9
+                addEdge(from.getName(), to.getName(), peso);
+            }
+            totalIntentos++;
         }
     }
 }
---
+++ Ejercicio1/ejercicio1.java
@@ -0,0 +1,7 @@
+package Ejercicio1;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        
+    }
+}
