87d1b0f0bdb3d52d7c468f49b25588fa4ac29f71
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-10-06 22:32:29 -0500

    haciendo la clase para el grafo

--- Ejercicio1/Graph.java
+++ Ejercicio1/Graph.java
@@ -1,5 +1,48 @@
 package Ejercicio1;
 
+import java.util.List;
+
 public class Graph {
+    private List<Node> nodes;
+    private List<Edge> edges;
+
+    public Graph(List<Node> nodes, List<Edge> edges) {
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public void addNode(String name) {
+        Node node = new Node(name);
+        if (!nodes.contains(node)) {
+            nodes.add(node);
+        }
+    }
+
+    public void addEdge(String from, String to, int weight) {
+        Node n1 = getNode(from);
+        Node n2 = getNode(to);
+        if (n1 == null || n2 == null) {
+            System.out.println("Uno de los nodos no existe.");
+            return;
+        }
+        edges.add(new Edge(n1, n2, weight));
+    }
+
+    private Node getNode(String name) {
+        for (Node n : nodes) {
+            if (n.getName().equals(name)) return n;
+        }
+        return null;
+    }
     
+    public void showGraph() {
+        System.out.println("Nodos:");
+        for (Node n : nodes) {
+            System.out.println(" - " + n);
+        }
+        System.out.println("Aristas:");
+        for (Edge e : edges) {
+            System.out.println(" - " + e);
+        }
+    }
 }
